ce6f037c4a4db0c2a482cb76a54a21c90ce51a65
sofianeSghiouar <sofianesgh@gmail.com>
2022-03-14 19:25:27 +0100

    first commit

---
+++ src/config/mongoose/default.config.ts
@@ -0,0 +1,5 @@
+export default {
+  id: "default",
+  url: process.env.DEFAULT_URL || "mongodb+srv://test:test1234@task-manager.9rwdw.mongodb.net/task-manager?retryWrites=true&w=majority",
+  connectionOptions: {}
+};
