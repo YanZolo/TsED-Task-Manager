2939abfddb97339ea8e2345834e0110931aa53b7
sofianeSghiouar <sofianesgh@gmail.com>
2022-03-15 14:16:38 +0100

    ci: refactore

--- src/controllers/rest/taskController.ts
+++ src/controllers/rest/taskController.ts
@@ -1,56 +1,59 @@
 import { Controller } from "@tsed/di";
-import { Delete, Description, Get, Patch, Post, Returns } from "@tsed/schema";
+import { Delete, Description, Get, Patch, Post, Returns, string } from "@tsed/schema";
 import { BodyParams, PathParams, QueryParams } from "@tsed/platform-params";
-import {TaskModel} from '../../models/taskModel';
-import { TasksService } from "src/services/tasksServices";
+import { TaskModel } from '../../models/taskModel';
+import { TasksServices } from "src/services/tasksServices";
+import { Request } from "express";
+import { Req } from "@tsed/common";
 
 
 @Controller("/tasks")
-export class UsersController {
-  constructor(private readonly tasksService: TasksService) {}
-  
+export class TasksControllerController {
+  constructor(private readonly tasksService: TasksServices) { }
+
   @Get("/")
   @Returns(200, Array).Of(TaskModel)
   @Description("return all tasks from database")
   async getAllTasks(): Promise<TaskModel[]> {
-    return [];
+    return this.tasksService.findAll();
   }
 
-  @Get("/:id")
+  @Get("/:name")
   @Returns(200)
   async getTask(
-    @PathParams("id")
+    @PathParams("name")
     @QueryParams("search")
-    @Description("Search keyword and return the task in database that match the given id")
-    id: string
+    @Description("Search keyword and return the task in database that match the given name")
+    name: string
   ) {
-    return { idRequestedTask: id };
+    return this.tasksService.findOne(name);
   }
 
   @Post("/")
   @Returns(201)
   @Description("add new task in database")
-  addTask(@BodyParams("task") body: TaskModel): TaskModel {
-    console.log("task", body.name);
-    return body;
+  async addTask(@BodyParams("task") task: TaskModel): Promise<string> {
+    console.log(`task: ${task.name}\n completed: ${task.completed}`);
+    return this.tasksService.create(task);
   }
 
-  @Patch("/:id")
+  @Patch("/:name")
   @Returns(200)
   async updateTask(
-    @PathParams("id")
+    @PathParams("name")
+    @BodyParams("task")
     @Description("find by id a task in database and update it")
-    id: string
+    name: string, task: TaskModel
   ) {
-    return { idTaskRequestedForUpdating: id };
+    return this.tasksService.update(name, task)
   }
 
-  @Delete("/:id")
+  @Delete("/:name")
   async deleteTask(
-    @PathParams("id")
-    @Description("find by id and delete task")
-    id: string
-  ): Promise<void> {
-    console.log("task deleted successfully");
+    @PathParams("name")
+    @Description("find by name and delete task")
+    name : string
+  ): Promise<string> {
+    return this.tasksService.delete(name)
   }
 }
--- src/services/tasksServices.ts
+++ src/services/tasksServices.ts
@@ -4,11 +4,12 @@ import { TaskModel } from "src/models/taskModel";
 
 
 @Service()
-export class TasksService {
+export class TasksServices {
     private readonly tasks: TaskModel[] = [];
 
     create(task: TaskModel) {
         this.tasks.push(task);
+        return `task ${task} was successfully created`
     }
 
     findAll(): TaskModel[] {
@@ -17,11 +18,20 @@ export class TasksService {
     findOne(name: string): TaskModel {
         return this.tasks.filter(task => task.name === name)[0]
     }
-    delete(name: string): TaskModel[] {
+    update(name: string, body: TaskModel): TaskModel {
         return this.tasks
             .splice(this.tasks
                 .indexOf(this.tasks
+                    .filter(task => task.name === name)[0]), 1, body)[0]
+    }
+    delete(name: string): string {
+        const result = this.tasks
+            .splice(this.tasks
+                .indexOf(this.tasks
                     .filter(task => task.name === name)[0]), 1)
+        console.log(`task ${result[0].name} was successfully deleted`);
+        return `task ${result[0].name} was successfully deleted `
+
         // const task =  this.tasks.filter(task => task.name === name)[0]
         // this.tasks.splice(this.tasks.indexOf(task),1)
         // return this.tasks
